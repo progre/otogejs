94cafee2b9fb48193e5168f78e6458384f09d1b1
progre <djyayutto@gmail.com>
2013-12-13 07:50:42 +0900

    add: createjs

--- src/public/javascript/main.ts
+++ src/public/javascript/main.ts
@@ -1,8 +1,10 @@
-/// <reference path="../../DefinitelyTyped/angular-1.0.d.ts"/>
+/// <reference path="../../typings/angularjs/angular.d.ts"/>
+/// <reference path="../../typings/angularjs/angular-route.d.ts"/>
+/// <reference path="../../typings/easeljs/easeljs.d.ts"/>
 
 var app = angular.module('app', ['ngRoute', 'ngAnimate']);
 app.config(['$routeProvider',
-    ($routeProvider: ng.IRouteProvider) => {
+    ($routeProvider: ng.route.IRouteProvider) => {
         $routeProvider
             .when('/', {
                 templateUrl: 'html/index.html', controller: 'IndexController'
@@ -13,6 +15,13 @@ app.config(['$routeProvider',
 ]);
 app.controller('IndexController',
     [() => {
+        var canvas = <HTMLCanvasElement>angular.element('#main-canvas')[0];
+        var stage = new createjs.Stage(canvas);
+        createjs.Ticker.timingMode = createjs.Ticker.RAF;
+        createjs.Ticker.addEventListener('tick', () => {
+            console.log('hoge');
+            stage.update();
+        });
     }]);
 
 angular.bootstrap(<any>document, ['app']);
